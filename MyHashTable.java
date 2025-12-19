e505556319bedcdba2e983500a98a6f0b4236b8c
Hrudaya Guttikonda <hrudaya_g@Hrudayas-MacBook-Air.local>
2025-12-19 16:37:27 +0530

    Added uc2 for hash table problem

--- MyHashTable.java
+++ MyHashTable.java
@@ -32,19 +32,33 @@ public class MyHashTable<K, V> {
             list.display();
     }
     public static void main(String[] args) {
+        //uc1
         String sentence = "To be or not to be";
-        String[] words = sentence.toLowerCase().split(" ");
+        String[] words1 = sentence.toLowerCase().split(" ");
 
-        MyHashTable<String, Integer> table = new MyHashTable<>();
+        MyHashTable<String, Integer> table1 = new MyHashTable<>();
 
-        for (String word : words) {
-            MyMapNode<String, Integer> node = table.get(word);
+        for (String word : words1) {
+            MyMapNode<String, Integer> node = table1.get(word);
             if (node == null)
-                table.add(word, 1);
+                table1.add(word, 1);
             else
-                table.add(word, node.value + 1);
+                table1.add(word, node.value + 1);
         }
-
-        table.display();
+        System.out.println("UC1 Output:");
+        table1.display();
+        //uc2
+        String para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words2 = para.toLowerCase().split(" ");
+        MyHashTable<String, Integer> table2 = new MyHashTable<>();
+        for (String word : words2) {
+            MyMapNode<String, Integer> node = table2.get(word);
+            if (node == null)
+                table2.add(word, 1);
+            else
+                table2.add(word, node.value + 1);
+        }
+        System.out.println("\nUC2 Output:");
+        table2.display();
     }
 }
